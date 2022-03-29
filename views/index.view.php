2942eede70526b52263b618d168b0e30ed5fed5c
Woopicus <bart_drongelen@hotmail.com>
2022-03-29 13:19:06 +0200

    3.0

--- views/index.view.php
+++ views/index.view.php
@@ -14,7 +14,16 @@
     </nav>
     <hi>My Tasks</h1>
 
-
+    <?php foreach ($tasks as $task) : ?>
+        <li>
+            <?php if ($task->completed) : ?>
+                <strike><?= $task-> description; ?></strike>
+            <?php else: ?>
+                <?= $task->description; ?>
+            <?php endif; ?>
+        </li>
+     <?php endforeach; ?>
+    
 </body>
 </html>
 
